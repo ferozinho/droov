ef3038fb474f9892712bb991470b2be0df44f780
ferozinho <feroz-chand@outlook.com>
2026-04-10 14:39:37 +0530

    assign c to nearest driver

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ from enum import Enum
 import uuid
 import datetime
 import json
+import random
 class ConsignmentStatus(Enum):
     ORDER_PLACED = "order_placed"
     ORDER_ASSIGNED = "order_assigned"
@@ -9,35 +10,49 @@ class ConsignmentStatus(Enum):
     ORDER_DISPATCHED = "order_dispatched"
     ORDER_DELIVERED = "order_delivered"
 
+class DriverStatus(Enum):
+    AVAILABLE = "available"
+    DELIVERING = "delivering"
+    UNAVAILABLE = "unavailable"
+
 class Consignment:
-    def __init__(self, c_details:json = None):
-        self.c_id = uuid.uuid4()
-        self.c_details = c_details
-        self.status = ConsignmentStatus.ORDER_PLACED
-        self.created_at = datetime.datetime.now()
-        self.updated_at = None
-        self.logs = [f"order placed at:{self.created_at}"]
-        self.is_active = True
-        
+    def __init__(self, c_details:dict = None):
+        self.c_id: int = uuid.uuid4()
+        self.c_details : dict | None = c_details
+        self.status : ConsignmentStatus = ConsignmentStatus.ORDER_PLACED
+        self.created_at : datetime = datetime.datetime.now()
+        self.updated_at : datetime.datetime | None = None
+        self.logs : list = [f"order placed at:{self.created_at}"]
+        self.is_active : bool = True
+        self.origin = {"x": random.randint(10,99),"y": random.randint(10,99)}
+        self.destination = {"x": random.randint(10,99),"y": random.randint(10,99)}
     def __repr__(self):
-        return f"id:{self.c_id}"
+        attrs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f"{self.__class__.__name__}({attrs})"
 
 class Driver:
     def __init__(self, name:str, id:int):
         self.name = name
         self.id = id
+        self.status : DriverStatus = DriverStatus.AVAILABLE
         self.is_active = True
+        self.cords = {"x": random.randint(10,99),"y": random.randint(10,99)}
+        
     def __repr__(self):
-        return (f"Name: {self.name}, id: {self.id},is_active: {self.is_active}")
+        attrs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f"{self.__class__.__name__}({attrs})"
     
 def get_drivers(size:int):
     drivers = []
-    for i in range(0,size):
+    for i in range(1,size-1):
         drivers.append(Driver(f"driver{i}",i))
     return drivers
 
 def get_consignments(size:int):
     consignments = []
-    for i in range(0,size):
+    for i in range(1,size-1):
         consignments.append(Consignment())
     return consignments
+
+consignment = Consignment()
+print(consignment)
--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 from classes import get_consignments,get_drivers,Consignment
+import math
 class ConsignmentQueue:
     MAX_LENGTH = 100
     def __init__(self):
@@ -11,7 +12,7 @@ class ConsignmentQueue:
     def isFull(self):
         return self.top==self.MAX_LENGTH-1
     
-    def bulk_enqueue(self,c_list:list[Consignment]):
+    def bulk_enqueue(self,c_list):
         list_size = len(c_list)
         if self.top+list_size == self.MAX_LENGTH-1:
             print(f"overflow.. you can only fill {self.MAX_LENGTH-self.top-2}")
@@ -33,7 +34,7 @@ class ConsignmentQueue:
             print("underflow")
             return
         dequeued_assignment = self.queue.pop(0)
-        top-=1
+        self.top-=1
         return dequeued_assignment
     
     def peek(self):
@@ -55,7 +56,21 @@ class Engine:
     consignments = get_consignments(50)
     c_queue = ConsignmentQueue()
     c_queue.bulk_enqueue(consignments)
+    consignment = c_queue.dequeue()
+    cords = consignment.origin
+    nearest_driver_dist = 100000
+    driver_assigned = None
+    for i in drivers:
+        driver_dist = math.sqrt(
+            ((cords["x"]-i.cords["x"])**2)+
+            ((cords["y"]-i.cords["y"])**2)
+            )
+        if nearest_driver_dist > driver_dist:
+            nearest_driver_dist = driver_dist
+            driver_assigned = i
 
+    print(nearest_driver_dist)
+    print(driver_assigned)
 engine = Engine()
 print(engine)
 
